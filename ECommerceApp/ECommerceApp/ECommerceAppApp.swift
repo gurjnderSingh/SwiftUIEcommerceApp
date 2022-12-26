a5a5da66102bd63b6ff83d5c162563091dc64e89
Gurjinder Singh <gurjindersingh663@gmail.com>
2022-12-26 22:33:50 +0530

    initial project created

---
+++ ECommerceApp/ECommerceApp/ECommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceAppApp.swift
+//  ECommerceApp
+//
+//  Created by Gurjinder Singh on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
