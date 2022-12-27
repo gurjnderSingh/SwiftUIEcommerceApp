b221e84563b2044c2c5014b6f81c2235d658bbba
Gurjinder Singh <gurjindersingh663@gmail.com>
2022-12-27 12:15:55 +0530

    footerView added

---
+++ ECommerceApp/ECommerceApp/Utility/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  ECommerceApp
+//
+//  Created by Gurjinder Singh on 27/12/22.
+//
+
+import SwiftUI
+
+//Data
+//Color
+let colorBackground = Color("ColorBackground")
+let colorGray = Color(UIColor.systemGray)
+//Layout
+//UX
+//Image
+//Font
+//String
+//Misc
+//API
---
+++ ECommerceApp/ECommerceApp/View/Home/FooterView.swift
@@ -0,0 +1,39 @@
+//
+//  FooterView.swift
+//  ECommerceApp
+//
+//  Created by Gurjinder Singh on 27/12/22.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Text("We offer the most cutting edge, comfortable, lightweight and durable football helmets in the market at affordable prices.")
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .layoutPriority(2)
+            
+            Image("logo-lineal")
+                .renderingMode(.template)
+                .foregroundColor(.gray)
+                .layoutPriority(0)
+            
+            Text("Copyright© Gurjinder Singh\n All right resserved") .multilineTextAlignment(.center)
+                .font(.footnote)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .layoutPriority(1)
+        }
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
